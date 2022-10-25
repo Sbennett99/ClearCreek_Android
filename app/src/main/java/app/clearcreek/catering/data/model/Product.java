9231e142361d9f9aa41688ea106e14f8e6f1d215
rahulkonda96 <S545465@nwmissouri.edu>
2022-10-24 20:30:15 -0500

    Updated product and category files.

--- app/src/main/java/app/clearcreek/catering/data/model/Product.java
+++ app/src/main/java/app/clearcreek/catering/data/model/Product.java
@@ -11,6 +11,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+//written by Rahul Konda
+//Comitted by Rahul Konda
+
 public class Product extends ProductItem implements Parcelable {
 
     private String bread;
--- app/src/main/java/app/clearcreek/catering/data/model/ProductCategory.java
+++ app/src/main/java/app/clearcreek/catering/data/model/ProductCategory.java
@@ -3,6 +3,9 @@ package app.clearcreek.catering.data.model;
 import java.util.LinkedList;
 import java.util.List;
 
+//written by Rahul Konda
+//Comitted by Rahul Konda
+
 public class ProductCategory extends ProductItem {
     private final List<Product> products;
 
