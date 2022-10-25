abcdc09ceefc97ca4d9a37e0735776e258480968
Seth <54418905+Sbennett99@users.noreply.github.com>
2022-10-24 19:34:28 -0500

    Added comments and made changes to  Address location class

--- app/src/main/java/app/clearcreek/catering/data/model/AddressLocation.java
+++ app/src/main/java/app/clearcreek/catering/data/model/AddressLocation.java
@@ -2,18 +2,30 @@ package app.clearcreek.catering.data.model;
 
 import com.google.firebase.firestore.GeoPoint;
 
+/*
+ * Custom Java Class
+ *
+ * Allows creation of java objects for
+ * each restaurant location to be utilized/managed
+ * within the application
+ *
+ */
 public class AddressLocation {
     private String name;
     private String address;
     private GeoPoint geopoint;
 
+    // Basic  empty Class Constructor
     public AddressLocation() {}
 
+    // Basic Class Constructor for creation with location information
     public AddressLocation(String name, String address, GeoPoint geopoint) {
         this.name = name;
         this.address = address;
         this.geopoint = geopoint;
     }
+
+    // Getters and Setters for the Class
 
     public String getName() {
         return name;
